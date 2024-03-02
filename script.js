a4cdd6904eea5fcd97460cf85d8ddeeb01b1df63
arda10102002 <karimovardamehr@gmail.com>
2024-03-02 22:54:18 +0500

    izmeneniya

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const images=[
-    "/images/1.jpg",
-    "/images/2.jpg",
-    "/images/3.jpg",
-    "/images/4.jpg",
-    "/images/5.jpg",
-    "/images/6.jpg",
+    "./images/1.jpg",
+    "./images/2.jpg",
+    "./images/3.jpg",
+    "./images/4.jpg",
+    "./images/5.jpg",
+    "./images/6.jpg",
 ]
 
 let prevIcon=document.querySelector(".prev"),
